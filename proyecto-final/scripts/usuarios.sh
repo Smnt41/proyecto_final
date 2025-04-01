fdd814ec04ea97487f671d645b6e1ec22874fb34
Samantha <smnt.41@gmail.com>
2025-04-01 08:34:46 +0200

    script usuarios acabado

--- proyecto-final/scripts/usuarios.sh
+++ proyecto-final/scripts/usuarios.sh
@@ -10,11 +10,26 @@ sudo useradd admin1 tecnico1 auditor1
 sudo groupadd administracion tecnicos
 
 #Unir usuarios a grupos
-sudo usermod --append -G administracion admin1
-sudo usermod --append -G tecnicos tecnico1
+sudo usermod -aG administracion admin1
+sudo usermod -aG tecnicos tecnico1
 
 #Crear directorios
 sudo mkdir -p /tmp/empresa/admin /tmp/empresa/tecnicos /tmp/empresa/compartido
 
+#Aplicar permisos
 
+#setUID
+sudo chmod u+s /tmp/empresa/admin
+
+#setGID
+sudo chmod g+s /tmp/empresa/tecnicos
+
+#StickyBit
+sudo chmod +t /tmp/empresa/compartido
+
+#Setfacl permisos auditor1
+sudo setfacl -m u:auditor1:rwx /tmp/empresa/tecnicos
+
+#Mensaje final para saber si se ha ejecutado el script
+print("Script para gestión de usuarios finalizado")
 
